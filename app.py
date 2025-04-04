7c520aa09db4f3de935ec1700827ec24f86d88ac
Adarsh <anmold652@gmail.com>
2025-04-04 15:29:10 +0530

    changes

--- app.py
+++ app.py
@@ -17,25 +17,20 @@ def get_coordinates(location):
     }
     response = requests.get(url, headers=headers)
 
-    # print("Response Status Code:", response.status_code)
-    # print("Response Text:", response.text)
+   
 
     if response.status_code == 200 and response.json():
         data = response.json()[0]
         return data["lat"], data["lon"]
     else:
-        return None, None  # Return None to handle errors gracefully
+        return None, None  
 
 
 
 def make_request(lat,lon):
-    # apiKeys = "9d4f21c7b340c55773ddc3606354145d"
 
-
-    # Load environment variables from .env
     load_dotenv()
 
-    # Retrieve the API key
     api_key = os.getenv("WEATHER_API_KEY")
 
     print("API KEY IS ",api_key)
@@ -61,7 +56,7 @@ def make_request(lat,lon):
             "humidity": data["main"]["humidity"],
             "pressure": data["main"]["pressure"],
             "weather": data["weather"][0]["description"],
-            "weather_id": data["weather"][0]["id"],  # Useful for custom icons
+            "weather_id": data["weather"][0]["id"], 
             "wind_speed": data["wind"]["speed"],
             "wind_gust": data["wind"].get("gust", "N/A"),
             "cloudiness": data["clouds"]["all"],
@@ -81,35 +76,19 @@ def make_request(lat,lon):
     else:
         return jsonify({"error": "Weather data not found"}), response.status_code
 
-
 @app.route("/get_data_by_cordinate/<lat>/<lon>")
 def get_data(lat,lon):
-
     if lat is None or lon is None:
         return jsonify({"error": "Location not found"}), 404
-    
     return make_request(lat,lon)
-
-    
-
-
 
 @app.route('/get_data_by_location/<location>')
 def get_data_by_location(location):
 
     lat,lon = get_coordinates(location)
-
     if lat is None or lon is None:
         return jsonify({"error": "Location not found"}), 404
-    
     return make_request(lat,lon)
-
-    
-
-   
-
-    
-
 
 
 if __name__ == '__main__':
